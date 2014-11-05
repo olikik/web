314cf740fe72899feb970cae79fc0952c2a96611
unknown <siv@SIV_PC.digt.local>
2014-11-05 10:51:02 +0300

    Таблица синусов с интервалом градусов 10 и интервалом минут 6

--- lab4/index.php
+++ lab4/index.php
@@ -1,5 +1,27 @@
 <?php
+    var $i = 0;
+    var $x = 0;
+    var $m = 1;
+    var $s = 1;
+    echo('<h2>Таблица синусов с интервалом градусов 10 и интервалом минут 6</h2>');
+    echo('<table>');
+    echo('<tr><th>');
+    echo('Аргумент');
+    echo('</th><th>');
+    echo('Значение');
+    echo('</th></tr>');
+    
+    for ($i<=36){
+        $m = $m * 6;
+        $s = $i * 10;
+        $x = $s* 60 + $m;
+        echo('<tr><td>');
+        echo('sin(' . $s . '&deg; ' . $m . '&#8242;');
+        echo('</td><td>');
+        echo(sin(deg2rad($x)));
+        echo('</td></tr>');
+    };
 
-    echo('<h1>HELLO!</h1>');
+    echo('</table>');
 
 ?>
