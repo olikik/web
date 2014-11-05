c8516cefe1b7c23687df7dbe9c1c808d1203ede1
olgam299 <olgam299@yandex.ru>
2014-11-06 00:57:56 +0300

    лаб 4 задание 1

---
+++ lw41/func.php
@@ -0,0 +1,36 @@
+<?php
+	function printSinTable($intervDeg, $intervMin)
+	{
+		echo 
+		"<style type = 'text/css'>
+		table
+		{
+			border-collapse: collapse;
+		}
+		td
+		{
+			border: 1px solid;
+			padding: 5px;
+		}
+		</style>";
+	    echo "<table>
+		        <tr>
+				  <td>sin</td>";
+		for ($min = 0; $min <= 60; $min += $intervMin * 60)
+		{
+			echo "<td>".$min."</td>";
+		}
+		echo  "</tr>";
+		for ($deg = 0; $deg <= 90; $deg += $intervDeg)
+		{
+		    echo "<tr>";
+			echo "<td>".$deg."</td>";
+			for ($min = 0; $min <= 1; $min += $intervMin)
+			{
+			    $sin = round(sin(deg2rad($deg + $min)), 4);
+				echo "<td>".$sin."</td>";
+			}
+			 echo "</tr>";
+		}
+		echo "</table>";
+	}
---
+++ lw41/index.php
@@ -0,0 +1,5 @@
+<?php
+	require_once('func.php');
+	$intervDeg = 10;
+	$intervMin = 6/60;
+	printSinTable($intervDeg, $intervMin);	
