9bb2c1b3d2df949ccce6cb4b579813b7f0c2a98e
ilya2306 <ilya2306@inbox.ru>
2014-11-05 11:22:19 +0300

    Update index.php

--- lab4/index.php
+++ lab4/index.php
@@ -1,8 +1,7 @@
-﻿<?php
-    $i = 0;
+<?php
     $x = 0;
-    $m = 1;
-    $s = 1;
+    $m = 0;
+    $s = 0;
     echo('<h2>Таблица синусов с интервалом градусов 10 и интервалом минут 6</h2>');
     echo('<table>');
     echo('<tr><th>');
@@ -10,15 +9,16 @@
     echo('</th><th>');
     echo('Значение');
     echo('</th></tr>');  
-    for ($i<=36){
-        $m = $m * 6;
+    for ($i=0; $i<=36; $i++)
+    {
+        $m = $m + 6;
         $s = $i * 10;
         $x = $s* 60 + $m;
-        echo('<tr><td>');
-        echo('sin(' . $s . '&deg; ' . $m . '&#8242;');
-        echo('</td><td>');
+        echo('<tr><td> ');
+        echo('sin(' . $s . '&deg; ' . $m . '&#8242;)');
+        echo('</td><td> ');
         echo(sin(deg2rad($x)));
-        echo('</td></tr>');
+        echo('</td></tr> ');
     };
     echo('</table>');
 ?>
