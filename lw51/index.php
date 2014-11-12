a5eadf8d4a9ba35a20267db9a47f49784c426a94
lolikik <olgam299@yandex.ru>
2014-11-12 19:14:30 +0300

    лаб5

---
+++ lw51/index.php
@@ -0,0 +1,7 @@
+<?php
+    //echo "11111";die;
+    require_once('/include/func.php');
+    include('/template/head.html');
+    include('/template/form.html');
+    serverInfo();
+    include('/template/footer.html');
