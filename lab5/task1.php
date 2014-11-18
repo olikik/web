43ab2e8b3a2368008a6e02b781d87885ccd18071
ilya2306 <ilya2306@inbox.ru>
2014-11-18 09:45:08 +0300

    Update task1.php

--- lab5/task1.php
+++ lab5/task1.php
@@ -1,9 +1,9 @@
 <?php
 
-    echo 'Запрос' . '<br>';
+    echo 'Р—Р°РїСЂРѕСЃ' . '<br>';
     echo $_SERVER["REQUEST_URI"];
     echo '<br> ' . '<br>';
-    echo 'Ответ' . '<br>' . 'Content-Type: ';
+    echo 'РћС‚РІРµС‚' . '<br>' . 'Content-Type: ';
     echo substr($_SERVER["HTTP_ACCEPT"], 0, strpos($_SERVER["HTTP_ACCEPT"], ','));
     echo '<br>' . 'Query string =   \'';
     echo $_SERVER["QUERY_STRING"];
