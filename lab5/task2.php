a2bf03fffe08ba235e6e922ab5814180652dae27
ilya2306 <ilya2306@inbox.ru>
2014-11-18 09:45:30 +0300

    Update task2.php

--- lab5/task2.php
+++ lab5/task2.php
@@ -1,13 +1,13 @@
 <?php
     
-    echo 'Запрос' . '<br>';
+    echo 'Р—Р°РїСЂРѕСЃ' . '<br>';
     echo $_SERVER["SERVER_PROTOCOL"];
     echo ' ';
     echo $_SERVER["REQUEST_METHOD"];
     echo ' ';
     echo $_SERVER["REQUEST_URI"];
     echo '<br> ' . '<br>';
-    echo 'Ответ' . '<br>' . 'Content-Type: ';
+    echo 'РћС‚РІРµС‚' . '<br>' . 'Content-Type: ';
     echo substr($_SERVER["HTTP_ACCEPT"], 0, strpos($_SERVER["HTTP_ACCEPT"], ',')) . '<br>' . '<br>';
     echo "Hello, Dear {$_GET['name']}!";
 
