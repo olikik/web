791c23b278929395c24476c5725439e17b3302fc
ilya2306 <ilya2306@inbox.ru>
2014-11-24 20:00:48 +0300

    lab 5

--- lab5/task5.php
+++ lab5/task5.php
@@ -1,20 +1,26 @@
 <?php 
     $identifier = trim($_GET['identifier']);
-    if(isset($identifier)){
-        if(empty($identifier)){
+    if(isset($identifier))
+	{
+        if(empty($identifier))
+		{
             die('Ошибка! Передан пустой параметр');
         }
     }
-    else{
+    else
+	{
         die('Ошибка! Параметр не передан!');
     }
 
-    if(!ctype_alpha($identifier[0])){
+    if(!ctype_alpha($identifier[0]))
+	{
         die('Переданный параметр НЕ является последовательностью');
     }
   
-    for($i = 1; $i < strlen($identifier); $i++){
-        if(!ctype_alnum($identifier[i])){
+    for($i = 1; $i < strlen($identifier); $i++)
+	{
+        if(!ctype_alnum($identifier[i]))
+		{
             die('Переданный параметр НЕ является последовательностью');
         }
     }
