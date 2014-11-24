c91eb4f91f6241dce27c7cbeef16f18da4d67271
ilya2306 <ilya2306@inbox.ru>
2014-11-24 19:56:29 +0300

    lab 5

--- lab5/task3.php
+++ lab5/task3.php
@@ -1,3 +1,12 @@
+HTTP/1.1 200 OK
+Date: Mon, 24 Nov 2014 16:53:26 GMT
+Server: Apache/2.4.10 (Win32) OpenSSL/1.0.1i PHP/5.6.3
+X-Powered-By: PHP/5.6.3
+Content-Length: 141
+Keep-Alive: timeout=5, max=100
+Connection: Keep-Alive
+Content-Type: text/html; charset=UTF-8
+
 <?php
 
     foreach ($_SERVER as $arguments => $value)
